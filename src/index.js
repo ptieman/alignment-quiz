1c005812d7daa3f380054780f9ea17d9cf4c1e77
Presley (Alexis) <presleytieman@outlook.com>
2024-01-17 16:26:17 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Question from './questions/Question';
+import { Questions } from './questions/Questions';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Question questions={Questions}/>
   </React.StrictMode>
 );
 
