e7e19ead3be894f5f4bc0d7a2821657df0f96360
Presley (Alexis) <presleytieman@outlook.com>
2024-01-17 16:23:36 -0500

    Create Questions dummy data

---
+++ src/questions/Questions.jsx
@@ -0,0 +1,43 @@
+// Dummy data for questions
+export const Questions = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+  },
+  {
+    id: 2,
+    question: "Who is CEO of Tesla?",
+  },
+  {
+    id: 3,
+    question: "The iPhone was created by which company?",
+  },
+  {
+    id: 4,
+    question: "How many Harry Potter books are there?",
+  },
+  {
+    id: 5,
+    question: "How many planets are in the Solar System?",
+  },
+  {
+    id: 6,
+    question: "What is the most popular sport in Japan?",
+  },
+  {
+    id: 7,
+    question: "What is the largest country in the world?",
+  },
+  {
+    id: 8,
+    question: "What is the capital of Australia?",
+  },
+  {
+    id: 9,
+    question: "What is the capital of Canada?",
+  },
+  {
+    id: 10,
+    quesiton: "What is your favorite color?"
+  },
+]
